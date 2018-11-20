954ca01e8aa9f3a4e249e0eaf62a7e0116cb106e
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 15:27:39 +0400

    Console input

---
+++ 004_ConsoleInput/004_ConsoleInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _004_ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Input age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input salary: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name} Age: {age} Height: {height} Salary: {salary}$");
+            Console.ReadKey();
+        }
+    }
+}
