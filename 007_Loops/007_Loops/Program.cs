f03e2a4624495adca8ca7fbb6401d28d5e422c7f
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 16:21:15 +0400

    Loops

---
+++ 007_Loops/007_Loops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _007_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"i^2 = {i * i}");
+            }
+
+            // do loop
+            int j = 6;
+            do
+            {
+                Console.WriteLine(j);
+                j--;
+            } while (j > 0);
+
+            // while loop
+            int k = 6;
+            while (k > 0)
+            {
+                Console.WriteLine(k);
+                k--;
+            }
+
+            // break
+            for (int m = 0; m < 9; m++)
+            {
+                if (m == 5)
+                    break;
+                Console.WriteLine(m);
+            }
+
+            // continue
+            for (int n = 0; n < 9; n++)
+            {
+                if (n == 5)
+                    continue;
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
