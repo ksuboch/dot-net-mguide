90f6924482ef5914640e25c584018bd532352139
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 12:57:56 +0400

    Hello World

---
+++ 001_HelloWorld/001_HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+            Console.ReadKey();
+        }
+    }
+}
