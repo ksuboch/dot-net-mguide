432cf03c89a5f3a48f3796439f7a1e018b001f00
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 13:08:02 +0400

    Console build

---
+++ 002_ConsoleBuild/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleBuild
+{
+    class Program
+    {
+        private static void Main(string[] arg)
+        {
+            Console.Write(DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
