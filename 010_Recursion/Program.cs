7870d7f086db3ab4b8bf058d9d32c6f25d9afa3c
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 18:29:45 +0400

    Recursion

---
+++ 010_Recursion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _010_Recursion
+{
+    class Program
+    {
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        static int Fibonacci(int x)
+        {
+            if (x == 0)
+            {
+                return 0;
+            }
+            else if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(x - 1) + Fibonacci(x - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(1));
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(10));
+
+            Console.WriteLine(Fibonacci(1));
+            Console.WriteLine(Fibonacci(5));
+            Console.WriteLine(Fibonacci(10));
+
+            Console.ReadLine();
+        }
+    }
+}
