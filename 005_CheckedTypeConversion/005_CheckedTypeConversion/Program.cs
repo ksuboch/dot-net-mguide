c1fed7ce434916036e48bcdc84875a07e2f5c121
ksuboch <3dvapper.inbox@gmail.com>
2018-11-20 15:47:27 +0400

    Checking type conversion

---
+++ 005_CheckedTypeConversion/005_CheckedTypeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _005_CheckedTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 3;
+                int b = 600;
+                byte c = GetC(a, b);
+                Console.WriteLine($"c = {c}, c.Type {c.GetType().ToString()}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static byte GetC(int a, int b)
+        {
+            return checked((byte)(a + b));
+        }
+    }
+}
