9960fa1a110126a11bdcdd6a6058934104593ea5
ksuboch <3dvapper.inbox@gmail.com>
2018-11-21 14:31:20 +0400

    Properties

---
+++ 017_Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _017_Properties
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Age must be larger then 17");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ 017_Properties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _017_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Tom";
+            string personName = p.Name;
+
+            Account a = new Account { sum = 50 };
+            Console.WriteLine(a.ToString());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Account
+    {
+        public int sum { get; set; }
+    }
+}
