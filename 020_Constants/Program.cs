031dd1f2e37ed046dca88f135d106aa43c53078c
ksuboch <3dvapper.inbox@gmail.com>
2018-11-21 16:13:25 +0400

    Const and Readonly

---
+++ 020_Constants/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _020_Constants
+{
+    class MathLib
+    {
+        public const double PI = 3.14;
+        public const double E = 2.71;
+        public readonly double stored;
+
+        public MathLib(double str)
+        {
+            stored = str;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathLib.E);
+
+            MathLib m = new MathLib(1.2);
+            Console.WriteLine(m.stored);
+
+            Console.ReadLine();
+        }
+    }
+}
