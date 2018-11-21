d76a0ebfaf186f7927fb19f80a80fec792282cf6
ksuboch <3dvapper.inbox@gmail.com>
2018-11-21 16:31:59 +0400

    Operator overloading

---
+++ 021_OperatorOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _021_OperatorOverloading
+{
+    class Counter
+    {
+        public int Value { get; set; }
+
+        public static Counter operator +(Counter c1, Counter c2) => new Counter { Value = c1.Value + c2.Value };
+        public static bool operator >(Counter c1, Counter c2) => c1.Value > c2.Value;
+        public static bool operator <(Counter c1, Counter c2) => c1.Value < c2.Value;
+        public static bool operator true(Counter c1) => c1.Value != 0;
+        public static bool operator false(Counter c1) => c1.Value == 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c1 = new Counter { Value = 23 };
+            Counter c2 = new Counter { Value = 45 };
+
+            Console.WriteLine(c1 > c2);
+            Console.WriteLine((c1 + c2).Value);
+
+            Console.ReadLine();
+        }
+    }
+}
