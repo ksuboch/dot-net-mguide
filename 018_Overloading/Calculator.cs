56057161c23df6a940fe64b7a3e1b57100720e1e
ksuboch <3dvapper.inbox@gmail.com>
2018-11-21 15:45:15 +0400

    Overloading

---
+++ 018_Overloading/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _018_Overloading
+{
+    class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Resilt is {result}");
+        }
+
+        public void Add(int a, int b, int c)
+        {
+            int result = a + b + c;
+            Console.WriteLine($"Resilt is {result}");
+        }
+
+        public void Add(int a, int b, int c, int d)
+        {
+            int result = a + b + c + d;
+            Console.WriteLine($"Resilt is {result}");
+        }
+
+        public void Add(double a, double b)
+        {
+            double result = a + b;
+            Console.WriteLine($"Resilt is {result}");
+        }
+    }
+}
---
+++ 018_Overloading/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _018_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Add(1, 2); // 3
+            calc.Add(1, 2, 3); // 6
+            calc.Add(1, 2, 3, 4); // 10
+            calc.Add(1.2, 3.4); // 4.6
+
+            Console.ReadLine();
+        }
+    }
+}
